2abedae641df3fbeca8b9e04180a7dc242b1939e
Sergio Anaya <choptop12@gmail.com>
2025-07-05 22:24:02 -0600

    Configuración de variables y despliegue de servidor configurable

--- Chapter_1/export_env.sh
+++ Chapter_1/export_env.sh
@@ -4,3 +4,16 @@
 set -a
 source .env
 set +a
+
+terraform apply
+
+# Uso de raw para evitar comillas dobles
+export public_ip=$(terraform output -raw public_ip)
+
+export url="http://$public_ip:$TF_VAR_server_port"
+
+# Esperar 90 segundos antes de hacer el curl
+sleep 90
+
+curl "$url"
+
