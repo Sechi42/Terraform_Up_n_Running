6007f5d50abfcec30ebf1b24f6ed238e2ca36a1c
Sergio Anaya <choptop12@gmail.com>
2025-07-23 19:14:12 -0600

    feat: soporte de tags dinámicos y mejoras en módulos Chapter_5

--- Chapter_5/live/global/iam/main.tf
+++ Chapter_5/live/global/iam/main.tf
@@ -6,12 +6,3 @@ resource "aws_iam_user" "example" {
     for_each = toset(var.user_names)
     name     = each.value   
 }
-
-resource "aws_instance" "example" {
-    for_each = toset(var.instance_names)
-    ami      = "ami-0fb653ca2d3203ac1"
-    instance_type = "t2.micro"
-    tags = {
-        Name = each.value
-    }
-}
