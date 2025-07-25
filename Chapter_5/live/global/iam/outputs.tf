116d73e1a27e9a4761c37d3d45a590386570e155
Sergio Anaya <choptop12@gmail.com>
2025-07-24 21:01:03 -0600

    Agrega outputs avanzados: upper_names, bios, upper_roles y for_directive en IAM global

--- Chapter_5/live/global/iam/outputs.tf
+++ Chapter_5/live/global/iam/outputs.tf
@@ -3,7 +3,23 @@ output "all_users" {
     description = "The ARN for all the iam user created"
 }
 
-
 output "all_arns" {
     value = values(aws_iam_user.example)[*].arn
+}
+
+output "upper_names" {
+    value = [for name in var.user_names : upper(name) if length(name) < 5]
+}
+
+output "bios" {
+    value = [for name, role in var.hero_thousand_faces : "${name} is the ${role}"]
+}
+
+output "upper_roles" {
+    value = {for name, role in var.hero_thousand_faces : upper(name) => upper(role)}
+}
+
+output "for_directive" {
+    value = "%{ for name in var.user_names }${name}, %{ endfor }"
+  
 }
