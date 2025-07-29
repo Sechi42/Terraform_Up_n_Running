b343e0cc84645d034c039d0fce1a0f35dd811969
Sergio Anaya <choptop12@gmail.com>
2025-07-28 22:59:01 -0600

    Agrega outputs for_directive_index y neo_cloudwatch_policy_arn en IAM global

--- Chapter_5/live/global/iam/outputs.tf
+++ Chapter_5/live/global/iam/outputs.tf
@@ -23,3 +23,8 @@ output "for_directive" {
     value = "%{ for name in var.user_names }${name}, %{ endfor }"
   
 }
+
+output "for_directive_index" {
+    value = "%{for i, name in var.user_names }(${i}) ${name} %{ endfor }"
+
+}
